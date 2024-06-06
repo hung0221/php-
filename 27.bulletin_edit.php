94b963165123bece867b7c9ee32d8a48afc35a36
hung0221 <155549020+hung0221@users.noreply.github.com>
2024-06-07 00:32:08 +0800

    27.bulletin_edit.php

--- 27.bulletin_edit.php
+++ 27.bulletin_edit.php
@@ -13,20 +13,18 @@
         // 連接資料庫
         $conn = mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
 
-        // 構建刪除佈告的 SQL 語句
-        $sql = "delete from bulletin where bid='{$_GET["bid"]}'";
-        #echo $sql; // 測試用，顯示 SQL 語句
+        // 更新佈告資料的 SQL 語句
+        $sql = "update bulletin set title='{$_POST['title']}', content='{$_POST['content']}', time='{$_POST['time']}', type={$_POST['type']} where bid='{$_POST['bid']}'";
 
         // 執行 SQL 語句
         if (!mysqli_query($conn, $sql)) {
-            // 如果刪除失敗，顯示錯誤訊息
-            echo "佈告刪除錯誤";
+            // 如果更新失敗，顯示錯誤訊息並在3秒後跳轉到佈告列表頁面
+            echo "修改錯誤";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
         } else {
-            // 如果刪除成功，顯示成功訊息
-            echo "佈告刪除成功";
+            // 如果更新成功，顯示成功訊息並在3秒後跳轉到佈告列表頁面
+            echo "修改成功，三秒鐘後回到佈告欄列表";
+            echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
         }
-
-        // 在3秒後跳轉到佈告列表頁面
-        echo "<meta http-equiv=REFRESH content='3, url=11.bulletin.php'>";
     }
 ?>
